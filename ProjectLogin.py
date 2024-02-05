c7ee1ddbcec23adf854045a0ab83901f8e16b8d5
Mahamed Elhashmi <121352498+elhashmie@users.noreply.github.com>
2024-02-05 23:48:34 +0200

    Add files via upload
    Edits

---
+++ ProjectLogin.py
@@ -0,0 +1,131 @@
+import sys
+import MainMenu
+import Tables
+import mysql.connector
+import connect
+#---------------------------------------------------------------------------------------------------------
+def login_to_admin() : # Admin Login
+    print("\n")
+    print("|                       ~~  College of Science Library  ~~                   |")
+    print()
+    print("                              LOGIN                                     \n")
+    print("WARNING : Only three Attempts to login")
+    for attempts in range(0,3) :  
+        AdminID=input("\t  Enter AdminID : ")   
+        password=input("\t  Enter Password : ")
+        
+        print()
+        mycursor.execute("SELECT Password from AdminRecord where AdminID={0}".format("\'"+AdminID+"\'"))
+        result=mycursor.fetchone()
+        if result:
+            temp,=result 
+            if temp==password: 
+                print("\n\t\t    WELCOME {0}   \n ".format("\'"+AdminID+"\'"))
+                MainMenu.Adminmenu()
+                break
+            else :
+                print("\t INVALID PASSWORD OR USERNAME ! TRY AGAIN ")
+                print("\t {0}st attempt is over \n ".format(attempts + 1))
+                continue
+
+        else :
+            print("\t NO SUCH USERNAME ! TRY AGAIN ")
+            print("\t {0}st attempt is over \n ".format(attempts + 1))
+            continue
+
+    else :
+        print("\t Try again later \n ")
+        print("\t System off  \n ")
+        print("*---------------------------------------------------------------------------------* \n")
+#---------------------------------------------------------------------------------------------------------
+def login_to_user(): #User login
+    
+    print("\n")
+    print("|                       ~~ College of Science Library  ~~                   |")
+    print()
+    print("1.CREATE ACCOUNT")
+    print("2.LOGIN TO YOUR ACCOUNT")
+    ch=int(input("Enter choice-->"))
+    if ch==1:
+        data=()
+        UserId=input("Enter your UserId ")
+        UserName=input("Enter your Name ")
+        Password=input("Enter Password to be set:")
+        data=(UserId,UserName, Password,None)
+        query="INSERT INTO UserRecord VALUES (%s, %s,%s,%s)"
+        mycursor.execute(query,data)
+        mydb.commit()
+        mycursor.execute("SELECT UserId from UserRecord where UserId={0}".format("\'"+UserId+"\'"))
+        result=mycursor.fetchone()
+        if result:
+            print("Account successfully created")
+        else:
+            print("Account already Exists")
+        login_to_user()
+
+    elif ch==2:
+        print("WARNING : Only three Attempts to login at a time")
+        for attempts in range(0,3) :  
+            UserID=input("\t  Enter UserID : ")   
+                                                  
+            password=input("\t  Enter Password : ")
+            print()
+            mycursor.execute("SELECT Password from UserRecord where UserID={0}".format("\'"+UserID+"\'"))
+            result=mycursor.fetchone()
+            if result:
+                temp,=result 
+                if temp==password: 
+                    print("\n\t\t    WELCOME {0}   \n ".format("\'"+UserID+"\'"))
+                    MainMenu.Usermenu()
+                    break
+                else :
+                    print("\t INVALID PASSWORD OR USERNAME ! TRY AGAIN ")
+                    print("\t {0}st attempt is over \n ".format(attempts + 1))
+                    continue
+
+            else :
+                print("\t NO SUCH USERNAME ! TRY AGAIN ")
+                print("\t {0}st attempt is over \n ".format(attempts + 1))
+                continue
+
+        else :
+            print("\t Try again later \n ")
+            print("\t System off  \n ")
+            print("*---------------------------------------------------------------------------------* \n")
+    else:
+        print("Enter valid choice")
+        login_to_user()
+#---------------------------------------------------------------------------------------------------------       
+def menu() :
+    print("\n\n")
+    print("|*************************************************************************************|")
+    print("|                       ~~  College of Science Library  ~~                   |")
+    print("|*************************************************************************************|")
+    print("\n")
+    print("                 ======================= MENU =======================                \n")
+    print(" 1. Login as a ADMIN")
+    print(" 2. Login as a USER")
+    print(" 3. EXIT \n\n ") #exit
+
+    while True :
+        ch= input(" Select [ 1/2/3 ] : ")
+        print()
+        if ch== "1" :
+            login_to_admin()
+            break
+        elif ch== "2" :
+            login_to_user()
+            break
+        elif ch== "3" :
+            cancel_request = input(" DO YOU WISH TO EXIT... [yes/no ] :  ")
+            if cancel_request in ["yes","Yes","YES"] :
+                sys.exit()
+            break
+        else :
+            print(" INVALID COMMAND ")
+            print(" RETRY \n")
+            continue
+#--------------------------------------------------------------------------------------------------------- 
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="",database="Library")
+mycursor=mydb.cursor()
+menu()
